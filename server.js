4f1f5111387033f00844c58759e557a6dc8c462f
LJApple <2439061874@qq.com>
2017-05-11 16:45:56 +0800

    express-static install

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const experss = require('express');
 var server = experss();
 
 server.use('/a.html',function(req,res){
-	res.send('abd');
+	res.send({a:12,b:5});
+	res.end();
 });
 server.listen(8080);
